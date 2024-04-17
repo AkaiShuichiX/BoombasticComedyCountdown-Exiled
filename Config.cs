e28a88767a2145a9bfe4716fdd33bb3fb5d2366c
Thanit <88444937+THQuery@users.noreply.github.com>
2024-04-18 04:35:56 +0700

    Boomba sumbe Bomba Subwa

---
+++ Config.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+using Exiled.API.Interfaces;
+
+namespace GCD
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = false;
+
+        [Description("Enable or disable the timer system for the Grenade.")]
+        public bool EnableGrenadeTimer { get; set; } = true;
+        
+        [Description("Enable or disable the timer system for the Flash.")]
+        public bool EnableFlashTimer { get; set; } = true;
+        
+        [Description("The duration of the timer for the Grenade in seconds.")]
+        public int GrenadeTimerDuration { get; set; } = 6;
+
+        [Description("The duration of the timer for the Flash in seconds.")]
+        public int FlashTimerDuration { get; set; } = 6;
+        
+        [Description("The text to display when the grenade will explode.")]
+        public string GrenadeExplodeText { get; set; } = "<color=yellow>Grenade will explode in</color> <color=red>{0}</color> <color=yellow>seconds</color>";
+        
+        [Description("The text to display when the flashbang will explode.")]
+        public string FlashExplodeText { get; set; } = "<color=yellow>Flash will explode in</color> <color=red>{0}</color> <color=yellow>seconds</color>";
+        
+        [Description("The time in seconds before the grenade explodes.")]
+        public float GrenadeFuseTime { get; set; } = 0.1f;
+        
+        [Description("The time in seconds the grenade will concuss players.")]
+        public float GrenadeConcussDuration { get; set; } = 5f;
+        
+        [Description("The time in seconds the grenade will burn players.")]
+        public float GrenadeBurnDuration { get; set; } = 5f;
+        
+        [Description("The time in seconds before the flashbang explodes.")]
+        public float FlashFuseTime { get; set; } = 0.1f;
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Exiled.API.Enums;
+using Exiled.API.Features;
+using Exiled.API.Features.Items;
+using Exiled.Events.EventArgs;
+using Exiled.Events.EventArgs.Player;
+using UnityEngine;
+using Timer = System.Timers.Timer;
+
+namespace GCD
+{
+    public class Main : Plugin<Config>
+    {
+        public override string Name => "Boombastic Comedy Countdown";
+        public override string Author => "Akai";
+        public override Version Version => new Version(1, 0, 5);
+
+        public override void OnEnabled()
+        {
+            Exiled.Events.Handlers.Player.ThrownProjectile += OnThrownProjectile;
+            Exiled.Events.Handlers.Player.ThrowingRequest += OnThrowingRequest;
+
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Player.ThrownProjectile -= OnThrownProjectile;
+            Exiled.Events.Handlers.Player.ThrowingRequest -= OnThrowingRequest;
+
+            base.OnDisabled();
+        }
+
+        /// <summary>
+        /// System to handle the timers for the objects.
+        /// </summary>
+        private readonly Dictionary<Player, Timer> _timers = new Dictionary<Player, Timer>();
+        private readonly Dictionary<Player, CancellationTokenSource> _tokenSources = new Dictionary<Player, CancellationTokenSource>();
+
+        private void OnThrowingRequest(ThrowingRequestEventArgs ev)
+        {
+            if (Config.Debug)
+            {
+                Debug.Log($"OnThrowingRequest: {ev.RequestType}");
+            }
+
+            if (ev.RequestType == ThrowRequest.BeginThrow)
+            {
+                if (_tokenSources.ContainsKey(ev.Player))
+                {
+                    _tokenSources[ev.Player].Cancel();
+                    _tokenSources[ev.Player].Dispose();
+                }
+
+                CancellationTokenSource tokenSource = new CancellationTokenSource();
+                _tokenSources[ev.Player] = tokenSource;
+
+                if (_timers.ContainsKey(ev.Player))
+                {
+                    _timers[ev.Player].Stop();
+                    _timers[ev.Player].Dispose();
+                }
+
+                int timerDuration = 0;
+                if (ev.Player.CurrentItem.Type == ItemType.GrenadeHE && Config.EnableGrenadeTimer)
+                {
+                    timerDuration = Config.GrenadeTimerDuration * 1000; // Fucker
+                }
+                else if (ev.Player.CurrentItem.Type == ItemType.GrenadeFlash && Config.EnableFlashTimer)
+                {
+                    timerDuration = Config.FlashTimerDuration * 1000; // Fucker
+                }
+
+                if (timerDuration > 0)
+                {
+                    Timer timer = new Timer(timerDuration);
+                    timer.Elapsed += (sender, e) => OnTimerElapsed(ev.Player);
+                    timer.AutoReset = false;
+                    timer.Start();
+                    
+                    _timers[ev.Player] = timer;
+
+                    // Start the countdown
+                    CountdownTask(ev.Player, timerDuration / 1000, tokenSource.Token); // Fucker
+
+                    if (Config.Debug)
+                    {
+                        Debug.Log($"Timer started for player {ev.Player.Nickname} with duration {timerDuration / 1000} seconds");
+                    }
+                }
+            }
+            else if (ev.RequestType == ThrowRequest.CancelThrow)
+            {
+                if (_tokenSources.ContainsKey(ev.Player))
+                {
+                    _tokenSources[ev.Player].Cancel();
+                    _tokenSources[ev.Player].Dispose();
+                    _tokenSources.Remove(ev.Player);
+                }
+
+                if (_timers.ContainsKey(ev.Player))
+                {
+                    _timers[ev.Player].Stop();
+                    _timers[ev.Player].Dispose();
+                    _timers.Remove(ev.Player);
+                }
+            }
+        }
+
+        private void OnThrownProjectile(ThrownProjectileEventArgs ev)
+        {
+            if (_timers.ContainsKey(ev.Player))
+            {
+                _timers[ev.Player].Stop();
+                _timers[ev.Player].Dispose();
+                _timers.Remove(ev.Player);
+            }
+        }
+
+        private async Task CountdownTask(Player player, int countdown, CancellationToken token)
+        {
+            while (countdown > 0)
+            {
+                if (token.IsCancellationRequested)
+                {
+                    token.ThrowIfCancellationRequested();
+                }
+                string hintMessage = player.CurrentItem.Type == ItemType.GrenadeHE ? Config.GrenadeExplodeText : Config.FlashExplodeText;
+                player.ShowHint(string.Format(hintMessage, countdown--), 1f);
+
+                if (Config.Debug)
+                {
+                    Debug.Log($"Countdown for player {player.Nickname}: {countdown} seconds remaining");
+                }
+
+                await Task.Delay(1000, token); // Wait 1 sec
+            }
+        }
+
+        private void OnTimerElapsed(Player player)
+        {
+            if (player.CurrentItem.Type == ItemType.GrenadeHE)
+            {
+                player.RemoveHeldItem();
+                ExplosiveGrenade grenade = (ExplosiveGrenade)Item.Create(ItemType.GrenadeHE);
+                grenade.FuseTime = Config.GrenadeFuseTime;
+                grenade.SpawnActive(player.Position, player); // owner of object
+                grenade.ConcussDuration = Config.GrenadeConcussDuration;
+                grenade.BurnDuration = Config.GrenadeBurnDuration;
+            }
+            else if (player.CurrentItem.Type == ItemType.GrenadeFlash)
+            {
+                player.RemoveHeldItem();
+                FlashGrenade flash = (FlashGrenade)Item.Create(ItemType.GrenadeFlash);
+                flash.FuseTime = Config.FlashFuseTime;
+                flash.SpawnActive(player.Position, player); // owner of object
+            }
+            if (_timers.ContainsKey(player))
+            {
+                _timers[player].Dispose();
+                _timers.Remove(player);
+                if (Config.Debug)
+                {
+                    Debug.Log($"Timer elapsed for player {player.Nickname}");
+                }
+            }
+        }
+    }
+}
